e4e7f72383650ac4e309c1e9c438fd6f0dcb73d4
Пермяков Сергей <ps@rktv.ru>
2023-03-19 16:26:07 +0500

    lesson 1

--- src/Equation.php
+++ src/Equation.php
@@ -2,50 +2,36 @@
 
 final class Equation
 {
-    const PRECISE = 4;
-
     public static function solve(float $a, float $b, float $c): array
     {
-        if (self::equalToZero($a)) {
-            throw new InvalidArgumentException('"a" cannot be equal to 0');
+        if ($a.is_nan() || $b.is_nan() || $c.is_nan()) {
+            throw new InvalidArgumentException("arguments must not be NaN");
         }
 
-        if (self::equalToZero($b)) {
-            if (self::lessThanZero($c)) {
-                $x1 = sqrt(abs($c / $a));
-                $x2 = -$x1;
-            } elseif (self::equalToZero($c)) {
-                $x1 = $x2 = 0;
-            } else {
-                return [];
-            }
+        if ($a.is_infinite() || $b.is_infinite() || $c.is_infinite()) {
+            throw new InvalidArgumentException("arguments must not be infinite");
+        }
+
+        if (abs($a) <= PHP_FLOAT_EPSILON) {
+            throw new InvalidArgumentException("a = 0, it is not a square equation");
+        }
+
+        $D = $b * $b - 4 * $a * $c;
+
+        if ($D < -PHP_FLOAT_EPSILON) {
+            return [];
+        } else if (abs($D) <= PHP_FLOAT_EPSILON) {
+            $res = -$b / (2 * $a);
+            return [$res, $res];
         } else {
-            $d = $b * $b - 4 * $a * $c;
-            if (self::greaterThanZero($d)) {
-                $x1 = (-$b + sqrt($d)) / 2 * $a;
-                $x2 = (-$b - sqrt($d)) / 2 * $a;
-            } elseif (self::equalToZero($d)) {
-                $x1 = $x2 = (-$b) / 2 * $a;
-            } else {
-                return [];
-            }
+            $res1 = (-$b + sqrt(D)) / (2 * $a);
+            $res2 = (-$b - sqrt(D)) / (2 * $a);
+
+            return [$res1, $res2];
         }
-
-        return [$x1, $x2];
     }
 
-    private static function equalToZero(float $f): bool
-    {
-        return 0 === bccomp(number_format($f, 10), '0', self::PRECISE);
-    }
-
-    private static function greaterThanZero(float $f): bool
-    {
-        return 1 === bccomp(number_format($f, 10), '0', self::PRECISE);
-    }
-
-    private static function lessThanZero(float $f): bool
-    {
-        return -1 === bccomp(number_format($f, 10), '0', self::PRECISE);
+    function is_blank($value) {
+        return empty($value) && !is_numeric($value);
     }
 }
--- tests/EquationTest.php
+++ tests/EquationTest.php
@@ -4,30 +4,18 @@ use PHPUnit\Framework\TestCase;
 
 final class EquationTest extends TestCase
 {
-    public function testSolve(): void
+    // Написать тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
+    public function testTask3(): void
     {
-        // p3
-        $this->assertEmpty(
-            Equation::solve(1, 0, 1)
-        );
+        //arrange
+        $a = 1;
+        $b = 0;
+        $c = 1;
 
-        // p5
-        $this->assertEquals(
-            [1, -1], Equation::solve(1, 0, -1)
-        );
+        //act
+        $actual = Equation::solve($a, $b, $c);
 
-        // p7
-        $this->assertEquals(
-            [-1, -1], Equation::solve(1, 2, 1)
-        );
-
-        // p11
-        $this->assertEquals(
-            [-0.1002, -0.1002], Equation::solve(0.1, 2.004, 10.04)
-        );
-
-        // p9
-        $this->expectException(InvalidArgumentException::class);
-        Equation::solve(0.00001, 0, 0);
+        //assert
+        $this->assertThat($actual,is_blank($actual), "roots of equation");
     }
 }
