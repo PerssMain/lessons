86e0f462e49c3337304751a41204435bf8b20897
Пермяков Сергей <ps@rktv.ru>
2023-03-19 18:35:11 +0500

    lesson 1

--- tests/EquationTest.php
+++ tests/EquationTest.php
@@ -4,7 +4,6 @@ use PHPUnit\Framework\TestCase;
 
 final class EquationTest extends TestCase
 {
-    //тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
     public function testSolve1(): void
     {
         $this->assertEmpty(
@@ -12,7 +11,6 @@ final class EquationTest extends TestCase
         );
     }
 
-    //тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
     public function testSolve2(): void
     {
         $this->assertEquals(
@@ -20,7 +18,6 @@ final class EquationTest extends TestCase
         );
     }
 
-    //тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1).
     public function testSolve3(): void
     {
         $this->assertEquals(
@@ -28,14 +25,12 @@ final class EquationTest extends TestCase
         );
     }
 
-    //тест, который проверяет, что коэффициент a не может быть равен 0. В этом случае solve выбрасывает исключение
     public function testSolve4(): void
     {
         $this->expectException(InvalidArgumentException::class);
         Equation::solve(0.00001, 0, 0);
     }
 
-    //11 задание
     public function testSolve5(): void
     {
         $this->assertEquals(
